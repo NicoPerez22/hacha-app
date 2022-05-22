4ba32366f85a56e1641e840ee98a27a080260160
nicolasperez@apptivalo.com <nicolas.perez@lubee.com.ar>
2022-05-22 14:20:18 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,28 @@
-import { NgModule } from '@angular/core';
+import { RegisterComponent } from './pages/auth/register/register.component';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { AuthComponent } from './pages/auth/auth.component';
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'auth',  component: AuthComponent },
+  { path: 'auth/login', component: LoginComponent },
+  { path: 'auth/register', component: RegisterComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule
   ],
   providers: [],
